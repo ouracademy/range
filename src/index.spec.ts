8a7490151a6d0852f6c2555f49eab8c28c24517e
artmadeit <this.arthur.email@gmail.com>
2018-12-15 18:25:40 -0500

    feat: initial range

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,4 +1,29 @@
-import { Greeter } from "./index";
-test("My Greeter", () => {
-  expect(Greeter("Carl")).toBe("Hello Carl");
+import { range, Range } from "./index";
+
+describe("Range", () => {
+  const aRange = range(1, 10);
+
+  it("has a start", () => {
+    expect(aRange.start).toBe(1);
+  });
+
+  it("has an end", () => {
+    expect(aRange.end).toBe(10);
+  });
+
+  it("has toString()", () => {
+    expect(String(aRange)).toBe(`[1-10]`);
+  });
+
+  it("has isEmpty()", () => {
+    expect(aRange.isEmpty).toBeFalsy();
+    expect(range(10, 10).isEmpty).toBeTruthy();
+  });
+
+  it("includes", () => {
+    expect(aRange.includes(1)).toBeTruthy();
+    expect(aRange.includes(10)).toBeTruthy();
+    expect(aRange.includes(0)).toBeFalsy();
+    expect(aRange.includes(11)).toBeFalsy();
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+export class Range {
+  constructor(public start: number, public end: number) {}
+
+  public toString() {
+    return `[${this.start}-${this.end}]`;
+  }
+
+  get isEmpty() {
+    return this.start >= this.end;
+  }
+
+  public includes(arg: number): boolean {
+    return this.start <= arg && arg <= this.end;
+  }
+}
+
+export const range = (start: number, end: number) => new Range(start, end);
