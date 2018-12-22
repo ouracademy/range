c1430339c420d8fc9c07628debd2420a34c5d209
artmadeit <this.arthur.email@gmail.com>
2018-12-22 18:00:25 -0500

    docs: add centre jsdoc

--- src/centre/centre.ts
+++ src/centre/centre.ts
@@ -1,5 +1,16 @@
 import { Range } from '../range';
 
+/**
+ * ### Example (es module)
+ * ```js
+ * import { centre, range, emptyRange } from '@ouracademy/range'
+ * centre(range(1, 3)) // 2
+ * centre(range(1, 10)) // 5.5
+ * centre(emptyRange) // undefined (empty intervals doesn't support this concept)
+ * ```
+ * @param aRange
+ * @see midpoint (as an alias)
+ */
 const centre = (aRange: Range) =>
   aRange.isEmpty ? undefined : (aRange.start + aRange.end) / 2;
 
